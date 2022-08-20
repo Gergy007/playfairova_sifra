99996cbcf16ab44c1b1fad5acfc74d028167152b
jurko <jurko.gergel@gmail.com>
2022-08-20 20:30:14 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+const char* MAKRO = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+
+char* playfair_encrypt(const char* key, const char* text);
+char* playfair_decrypt(const char* key, const char* text);
+int main()
+{
+
+    return 0;
+}
+
+char* playfair_encrypt(const char* key, const char* text)
+{
+
+}
+
+char* playfair_decrypt(const char* key, const char* text)
+{
+
+}
